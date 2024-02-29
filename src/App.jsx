97a35fd0b8968a129a8bedeefa254bcbc2017650
Conor Alen <145404862+callen996@users.noreply.github.com>
2024-02-28 22:55:20 -0500

    created new files and updated its code

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import BucketList from './components/BucketList';
+
+function App() {
+  return (
+    <div className="employment-app">
+      <BucketList />
+    </div>
+  );
+}
+
+export default App;
