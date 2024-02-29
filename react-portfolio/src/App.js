2e1a41bd44b1f01ec22f2c4adad90fd9ea47a71b
Conor Alen <145404862+callen996@users.noreply.github.com>
2024-02-29 11:07:36 -0500

    created new routes and added their code

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -4,20 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Find your Genealogy" element={<Find your Genealogy />} />
+        <Route path="/Projects" element={<Projects />} />
+      </Routes>
+     </Router>
     </div>
   );
 }
